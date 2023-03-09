8ce02616f23a39c8463ddab170d2e66e39ccae7e
MozesJR <Mozessapari11@gmail.com>
2023-03-09 14:37:12 +0700

    Membuat ChatBox dengan ChatBot

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -17,6 +17,30 @@
 
 @yield('auth')
 
+{{-- Buat Bot --}}
+<div id="ChitchatWidget"></div>
+<script src="https://widget.eva.id/js/fg.js?v=VSI1.2"></script>
+<script language="javascript">
+    var set = new Array();
+    set["c_project"] = "167834243227437210";
+    set["c_button_float_bg"] = "#3a67c9";
+    set["c_button_float_title"] = "";
+    set["c_float_bg"] = "#3a67c9";
+    set["c_float_title"] = "Bot HARSIS";
+    set["c_float_title_color"] = "#ffffff";
+    set["c_float_image"] = "https://dashboard.eva.id/uploads/20230309061605541711.png";
+    (function(i, s, o, g, r, a, m) {
+        i["chitchat"] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, "script", "https://widget.eva.id/js/eva_lite.js?v=1.087", set)
+</script>
+
 <script src="{{ asset('assets/js/vendor-all.min.js') }}"></script>
 <script src="{{ asset('assets/js/plugins/bootstrap.min.js') }}"></script>
 <script src="{{ asset('assets/js/ripple.js') }}"></script>
